582c6878186c0c2b33926639bc2da28e176d5e25
Chirag <76880977+chirag127@users.noreply.github.com>
2023-02-15 15:27:45 +0000

    commit

--- app.py
+++ app.py
@@ -1,64 +1,58 @@
-import streamlit as st
 import requests
+import streamlit as st
 
-def process_url(url):
+
+def get_video_details(url):
     fxl = url.split("/")
     cid = fxl[-1]
-    URL=f'https://diskuploader.entertainvideo.com/v1/file/cdnurl?param={cid}'
+    URL = f"https://diskuploader.entertainvideo.com/v1/file/cdnurl?param={cid}"
     header = {
-        'Accept': '*/*',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Referer': 'https://mdisk.me/',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'
+        "Accept": "*/*",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Referer": "https://mdisk.me/",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36",
     }
     resp = requests.get(url=URL, headers=header).json()
-    fn = resp['filename']
-    dn = resp['display_name']
-    dr = resp['duration']
-    sz = resp['size']
-    ht = resp['height']
-    wt = resp['width']
-    download = resp['download']
-    return fn, dn, dr, sz, ht, wt, download
+    return resp
 
-def download_all(urls):
-    for url in urls:
-        # fn, dn, dr, sz, ht, wt, download = process_url(url)
-        filename, display_name, duration, size, height, width, download = process_url(url)
-        st.write(f"Filename: {fn}")
-        st.write(f"Display Name: {dn}")
-        st.write(f"Duration: {dr}")
-        st.write(f"Size: {sz}")
-        st.write(f"Height: {ht}")
-        st.write(f"Width: {wt}")
-        st.write(f"Download URL: {download}")
-        st.download_button("Download", download)
 
-def download_single(url):
-    fn, dn, dr, sz, ht, wt, download = process_url(url)
-    st.write(f"Filename: {fn}")
-    st.write(f"Display Name: {dn}")
-    st.write(f"Duration: {dr}")
-    st.write(f"Size: {sz}")
-    st.write(f"Height: {ht}")
-    st.write(f"Width: {wt}")
-    st.write(f"Download URL: {download}")
-    st.download_button("Download", download)
+def download_video(url):
+    st.write(f"Downloading {url}...")
+    st.markdown(
+        f'<a href="{url}" download="{url.split("/")[-1]}.mp4">Download Video</a>',
+        unsafe_allow_html=True,
+    )
+
 
 def app():
-    st.title("mdisk Downloader")
-    text = st.text_area("Enter the text content of the mdisk website", height=200)
-    urls = [url.strip() for url in text.split() if url.startswith("https://mdisk.me")]
-    st.write(f"Number of mdisk URLs found: {len(urls)}")
-    if st.button("Download All"):
-        download_all(urls)
-    if st.button("Download Single"):
-        for url in urls:
-            st.write(url)
-            if st.button(f"Process {url}"):
+    st.title("MDisk Video Downloader")
+    text_input = st.text_area("Enter the text content of the MDisk website:")
 
-                download_single(url)
+    if st.button("Extract URLs"):
+        urls = []
+        for line in text_input.split("\n"):
+            if line.startswith(" https://mdisk.me/convertor/"):
+                urls.append(line.strip())
 
-if __name__ == "__main__":
-    app()
+        st.write(f"Found {len(urls)} video URLs:")
+        for i, url in enumerate(urls):
+            st.write(f"{i+1}. {url}")
+
+        if st.button("Download"):
+            for url in urls:
+                resp = get_video_details(url)
+                fn = resp["filename"]
+                dn = resp["display_name"]
+                dr = resp["duration"]
+                sz = resp["size"]
+                ht = resp["height"]
+                wt = resp["width"]
+                download = resp["download"]
+                st.write(f"File Name: {fn}")
+                st.write(f"Display Name: {dn}")
+                st.write(f"Duration: {dr}")
+                st.write(f"Size: {sz}")
+                st.write(f"Height: {ht}")
+                st.write(f"Width: {wt}")
+                download_video(download)
