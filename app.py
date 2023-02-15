aa864bc0795f979fb3b1b90805541a091f793ee7
Chirag <76880977+chirag127@users.noreply.github.com>
2023-02-15 16:08:19 +0000

    test

--- app.py
+++ app.py
@@ -1,58 +1,43 @@
-import requests
 import streamlit as st
+import requests
+import json
 
+st.title("Mdisk Downloader")
 
-def get_video_details(url):
-    fxl = url.split("/")
+def get_urls(text):
+    urls = []
+    for line in text.split():
+        if line.startswith("https://mdisk.me"):
+            urls.append(line)
+    return urls
+
+def get_info(url):
+    inp = url #input('Enter the Link: ')
+    fxl = inp.split("/")
     cid = fxl[-1]
-    URL = f"https://diskuploader.entertainvideo.com/v1/file/cdnurl?param={cid}"
+    URL=f'https://diskuploader.entertainvideo.com/v1/file/cdnurl?param={cid}'
     header = {
-        "Accept": "*/*",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Referer": "https://mdisk.me/",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36",
+        'Accept': '*/*',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Referer': 'https://mdisk.me/',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'
     }
     resp = requests.get(url=URL, headers=header).json()
     return resp
 
+def open_url(url):
+    import webbrowser
+    webbrowser.open(url)
 
-def download_video(url):
-    st.write(f"Downloading {url}...")
-    st.markdown(
-        f'<a href="{url}" download="{url.split("/")[-1]}.mp4">Download Video</a>',
-        unsafe_allow_html=True,
-    )
+text = st.text_area("Enter the text")
 
-
-def app():
-    st.title("MDisk Video Downloader")
-    text_input = st.text_area("Enter the text content of the MDisk website:")
-
-    if st.button("Extract URLs"):
-        urls = []
-        for line in text_input.split("\n"):
-            if line.startswith(" https://mdisk.me/convertor/"):
-                urls.append(line.strip())
-
-        st.write(f"Found {len(urls)} video URLs:")
-        for i, url in enumerate(urls):
-            st.write(f"{i+1}. {url}")
-
-        if st.button("Download"):
-            for url in urls:
-                resp = get_video_details(url)
-                fn = resp["filename"]
-                dn = resp["display_name"]
-                dr = resp["duration"]
-                sz = resp["size"]
-                ht = resp["height"]
-                wt = resp["width"]
-                download = resp["download"]
-                st.write(f"File Name: {fn}")
-                st.write(f"Display Name: {dn}")
-                st.write(f"Duration: {dr}")
-                st.write(f"Size: {sz}")
-                st.write(f"Height: {ht}")
-                st.write(f"Width: {wt}")
-                download_video(download)
+if st.button("Process"):
+    urls = get_urls(text)
+    st.write(f"There are {len(urls)} urls")
+    st.write(urls)
+    for url in urls:
+        info = get_info(url)
+        st.write(info)
+        st.write(f"Downloading {info['filename']}")
+        open_url(info['download'])
